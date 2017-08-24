aa0fb7ba814ebbd86b6cb990b20ffe5f048182df
alkononovich <31285957+alkononovich@users.noreply.github.com>
2017-08-24 19:47:49 +0300

    Delete removeNums.java

--- removeNums.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class removeNums {
-    public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String s = reader.readLine();
-        System.out.print(s.replaceAll("\\d", ""));
-    }
-}
